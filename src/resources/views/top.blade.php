7a1ccd5ac94af2f682450c93270d401513b22b6f
白金愛里 <air.shirogane@gmail.com>
2025-03-10 05:42:53 +0900

    first commit

---
+++ src/resources/views/top.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="木製ジュエリーブランド Wooden Jewelry">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/favicon.ico">
+    <link rel="stylesheet" href="{{ asset('css/top.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <title>Wooden Jewelry</title>
+</head>
+
+<body>
+    test
+</body>
+</html>
